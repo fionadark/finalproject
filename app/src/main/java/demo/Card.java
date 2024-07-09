6ce5464948777b5bfaf0dc41ff60da7e8b387302
fionadark <brigiddark@gmail.com>
2024-07-09 15:40:25 -0700

    initial commit

---
+++ app/src/main/java/demo/Card.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Card {
+    public String name;
+    public String name_short;
+    public String value;
+    public int value_int;
+    public String suit;
+    public String type;
+    public String meaning_up;
+    public String meaning_rev;
+    public String desc;
+}
---
+++ app/src/main/java/demo/Spread.java
@@ -0,0 +1,7 @@
+package demo;
+import java.util.List;
+
+public class Spread {
+    public int nhits;
+    public List<Card> cards;
+}
